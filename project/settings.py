b6dd6595d4d88383cae8db3c980a8f6db5e76c4c
shubham3279 <shubhambseb123@gmail.com>
2022-10-06 18:29:30 +0530

    removed static dir form settings.py

--- project/settings.py
+++ project/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,7 +106,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
